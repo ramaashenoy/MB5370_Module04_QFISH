c61204a51409dd664f2796741301e46057dbf983
ramaashenoy <ramaa.shenoy@my.jcu.edu.au>
2024-09-09 11:19:13 +1000

    QFISH Net Fsihery data graph

---
+++ MB5370_Module04_QFISH.R
@@ -0,0 +1,13 @@
+#------------------------------------#
+#MB5370: Introduction to Data Science
+# Ramaa Shenoy
+# Workshop 3: 9th September, 2024
+#------------------------------------#
+
+##Workspace:
+#D:\Users\Desktop\Sem 3\Techniques in Marine Science 1 (MB5370)\Module 4
+
+#In this section, 
+
+#install.packages("usethis")
+library("usethis")
