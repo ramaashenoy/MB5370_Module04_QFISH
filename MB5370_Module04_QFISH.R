eaf7d9a6d0134b1a6f9d9e068575bb59ca585204
ramaashenoy <ramaa.shenoy@my.jcu.edu.au>
2024-09-10 12:29:55 +1000

    commit 10/09

--- MB5370_Module04_QFISH.R
+++ MB5370_Module04_QFISH.R
@@ -1,5 +1,5 @@
 #------------------------------------#
-#MB5370: Introduction to Data Science
+# MB5370: Introduction to Data Science
 # Ramaa Shenoy
 # Workshop 3: 9th September, 2024
 #------------------------------------#
@@ -7,8 +7,14 @@
 ##Workspace:
 #D:\Users\Desktop\Sem 3\Techniques in Marine Science 1 (MB5370)\Module 4
 
-#In this section, 
+#STEP-1: Committing the project to my GitHub repository
 
 #install.packages("usethis")
 library("usethis")
 usethis::use_github()
+
+#STEP-2: Tidying the data
+library(tidyverse)
+
+
+
